0cd1a682af4fce83042c37ab0c3232f3a791ccbf
zeng <zenghuang19@163.com>
2022-01-04 16:41:11 +0800

    add:增加验证规则

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"goblog/app/models/user"
+	"goblog/app/requests"
 	"goblog/pkg/view"
 	"net/http"
 )
@@ -19,23 +20,32 @@ func (*AuthController) Register(w http.ResponseWriter, r *http.Request)  {
 
 // DoRegister 注册逻辑
 func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request)  {
-	//0.初始化变量
-	name := r.PostFormValue("name")
-	email := r.PostFormValue("email")
-	password := r.PostFormValue("password")
-	// 1. 表单验证
-	// 2. 验证通过 —— 入库，并跳转到首页
+	// 1. 初始化数据
 	_user := user.User{
-		Name: name,
-		Email: email,
-		Password: password,
+		Name:            r.PostFormValue("name"),
+		Email:           r.PostFormValue("email"),
+		Password:        r.PostFormValue("password"),
+		PasswordConfirm: r.PostFormValue("password_confirm"),
 	}
-	_user.Create()
-	if _user.ID > 0 {
-		fmt.Fprint(w, "插入成功 ID为" +_user.GetStringID())
+	// 2.表单规则
+	errs := requests.ValidateRegistrationForm(_user)
+	if len(errs) > 0 {
+		//验证未通过
+		view.RenderSimple(w, view.D{
+			"Errors": errs,
+			"User": _user,
+		}, "auth.register")
 	}else {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, "创建用户失效，请联系管理员")
+		//  验证通过 —— 入库，并跳转到首页
+		_user.Create()
+		if _user.ID > 0 {
+			fmt.Fprint(w, "插入成功 ID为" +_user.GetStringID())
+		}else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "创建用户失效，请联系管理员")
+		}
 	}
-	// 3. 表单不通过 —— 重新显示表单
+
+	// 5. 表单不通过 —— 重新显示表单
+
 }
