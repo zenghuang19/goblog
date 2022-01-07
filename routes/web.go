a7f8b5881fac9d9f8666c7c700a8aa4f70abeb3b
zeng <zenghuang19@163.com>
2022-01-07 15:33:59 +0800

    add:首页

--- routes/web.go
+++ routes/web.go
@@ -12,11 +12,11 @@ func RegisterWebRoutes(r *mux.Router) {
 	// 静态页面
 	pc := new(controllers.PagesController)
 	r.NotFoundHandler = http.HandlerFunc(pc.NotFound)
-	r.HandleFunc("/", pc.Home).Methods("GET").Name("home")
 	r.HandleFunc("/about", pc.About).Methods("GET").Name("about")
 
 	//文章相关页面
 	ac := new(controllers.ArticlesController)
+	r.HandleFunc("/", ac.Index).Methods("GET").Name("home")
 	r.HandleFunc("/articles/{id:[0-9]+}", ac.Show).Methods("GET").Name("articles.show")
 
 	//文章列表
