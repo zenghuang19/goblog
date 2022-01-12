b92663c2ebf934d89ba998e8b57e8a6e5c041f3b
zeng <zenghuang19@163.com>
2022-01-12 14:49:04 +0800

    add:增加退出登录操作

--- routes/web.go
+++ routes/web.go
@@ -48,6 +48,8 @@ func RegisterWebRoutes(r *mux.Router) {
 	r.HandleFunc("/auth/login", auc.Login).Methods("GET").Name("auth.login")
 	r.HandleFunc("/auth/dologin", auc.DoLogin).Methods("POST").Name("auth.dologin")
 
+	r.HandleFunc("/auth/logout", auc.Logout).Methods("POST").Name("auth.logout")
+
 	//中间件：强制内容类型为 HTML
 	//r.Use(middlewares.ForceHTML)
 
